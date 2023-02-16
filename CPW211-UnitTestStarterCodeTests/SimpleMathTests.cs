b1f62dab26d1800c554ed8761294fb35a4d5da26
Isaiah C <95327043+theisaiahcc@users.noreply.github.com>
2023-02-16 13:06:37 -0800

    completed unfinished and not started methods

--- CPW211-UnitTestStarterCodeTests/SimpleMathTests.cs
+++ CPW211-UnitTestStarterCodeTests/SimpleMathTests.cs
@@ -5,9 +5,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Security.Principal;
 
 namespace CPW211_UnitTestStarterCode.Tests
 {
+
     [TestClass()]
     public class SimpleMathTests
     {
@@ -19,14 +21,18 @@ namespace CPW211_UnitTestStarterCode.Tests
         public void Add_TwoNumbers_ReturnsSum(double num1, double num2)
         {
             // Use the DataRow values to test the Add method
-            Assert.Fail();
+            Assert.AreEqual(num1 + num2, SimpleMath.Add(num1, num2));
         }
 
         [TestMethod]
-        public void Multiply_TwoNumbers_ReturnsProduct()
+        [DataRow(5, 10)]
+        [DataRow(0, 100)]
+        [DataRow(-1, -10)]
+        [DataRow(0, -0)]
+        public void Multiply_TwoNumbers_ReturnsProduct(double a, double b)
         {
             // Use a few pairs of values to test the Multiply method
-            Assert.Fail();
+            Assert.AreEqual(a * b, SimpleMath.Multiply(a, b));
         }
 
         [TestMethod]
@@ -34,11 +40,21 @@ namespace CPW211_UnitTestStarterCode.Tests
         {
             // Divide by zero should throw an argument exception with a message
             // "Denominator cannot be zero"
-            Assert.Fail();
+            Assert.ThrowsException<ArgumentException>(() => SimpleMath.Divide(1, 0));
         }
 
         // TODO: Test Divide method with two valid numbers
+        [TestMethod]
+        public void Divide_TwoValidNumbers_ReturnsQuotient()
+        {
+            Assert.AreEqual(5, SimpleMath.Divide(50, 10));
+        }
 
         // TODO: Test subtract method with two valid numbers
+        [TestMethod]
+        public void Subtract_TwoValidNumbers_ReturnsDifference()
+        {
+            Assert.AreEqual(20, SimpleMath.Subtract(37, 17));
+        }
     }
 }
